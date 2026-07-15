85cee43885462983cd598d90d2e5cd1170efe01e
Pedro Orellana <pedrorellana21@gmail.com>
2026-07-14 23:06:56 -0700

    start work on creating main loop

--- main.c
+++ main.c
@@ -2,8 +2,36 @@
 
 // prototypes
 
-int main()
-{
+
+//account struct
+struct account {
+    char owner[20];
+    float balance;
+};
+
+int main() {
+    
+    int ret = 0;
+    struct account account1 = {.balance = 0, .owner = ""};
     printf("This is the Electronic bank app!\n");
+    
+    while(!ret) {
+
+        printf("Please provide your full name: \n");
+
+        ret = scanf("%19[^\n]", account1.owner);
+        if(!ret) {
+            printf("You did not provide your full name...\n");
+            getchar();
+        }
+
+        
+    }
+
+    
+    printf("Thank you, %s\n", account1.owner);
+  
+    
+   
     return 0;
 }
